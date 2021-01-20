cc7e7c39afcc45e95acecf041f2f7828ab275820
Korpatiy <tretiakovslava@gmail.com>
2021-01-20 21:38:40 +0500

    create my-container, move agents to it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,38 @@
-import jade.Boot;
-import jade.core.Agent;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
-import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 
 public class Main {
 
-    private static ContainerController myContainer;
+    private static Runtime runtime;
 
-    public static void createAgent() {
-        try {
-            AgentController rma = myContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
-            rma.start();
-            String[] args = new String[]{"ClassicTable"};
-            AgentController AgentDb = myContainer.createNewAgent("DistributorAgent-1",
-                    "Agents.Distributors.AgentDb", args);
-            AgentDb.start();
-            AgentController AgentDb1 = myContainer.createNewAgent("DistributorAgent-2",
-                    "Agents.Distributors.AgentDb1", null);
-            //  AgentDb1.start();
-        } catch (StaleProxyException e) {
-            e.printStackTrace();
-        }
+    public static void startMainContainer() throws StaleProxyException {
+        Profile myProfile = new ProfileImpl();
+        AgentContainer mainContainer = runtime.createMainContainer(myProfile);
+        AgentController rma = mainContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
+        rma.start();
     }
 
-    public static void main(String[] args) {
-        Runtime myRuntime = Runtime.instance();
-        Profile myProfile = new ProfileImpl();
-        myContainer = myRuntime.createAgentContainer(myProfile);
-        createAgent();
+    public static void startMyContainer() throws StaleProxyException {
+        Profile anotherProfile = new ProfileImpl(false);
+        anotherProfile.setParameter(Profile.CONTAINER_NAME,"Manufacture-Container");
+        AgentContainer anotherContainer = runtime.createAgentContainer(anotherProfile);
+
+        String[] args = new String[]{"ClassicTable"};
+        AgentController AgentDb = anotherContainer.createNewAgent("DistributorAgent-1",
+                "Agents.Distributors.AgentDb", args);
+        AgentDb.start();
+        AgentController testAgent = anotherContainer.createNewAgent("DistributorAgent-2",
+                "Agents.Distributors.AgentDb1", null);
+        testAgent.start();
+    }
+
+    public static void main(String[] args) throws StaleProxyException {
+        runtime = Runtime.instance();
+        startMainContainer();
+        startMyContainer();
     }
 }
